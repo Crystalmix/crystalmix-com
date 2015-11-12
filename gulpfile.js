c4e8230a5258d0d2c2b35624cf5760b65b74e28d
Sergey Gorshkov <sgorshkov@Crystalnixs-iMac-2.local>
2015-11-12 10:04:47 +0600

    Add dropdown menu on hover.

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,10 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 
 gulp.task('sass', function () {
-    gulp.src('./styles/sass/**/*.sass')
+    gulp.src('./src/css/sass/**/*.sass')
         .pipe(concat('styles.sass'))
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./styles/css'));
+        .pipe(gulp.dest('./src/css'));
 });
 
 gulp.task('default', function () {
