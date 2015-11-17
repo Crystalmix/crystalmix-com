7dd453fd389ad3b6f0d7382e53e44c18ab2a4ea0
Sergey Gorshkov <sgorshkov@Crystalnixs-iMac-2.local>
2015-11-17 16:16:07 +0600

    Add all content to slider.

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
-    var slider_mobile = $('.slider-mobile'),
-        slider_mobile_content = $('.bespoke__mobile__content'),
-        slide_name = $('.mobile__slide-name');
+    var slider_mobile = $('.slider-mobile');
 
-    var content_html = $.get('ajax/services/hybrid.html', function());
-
-    slide_name.text('Hybrid apps');
-    slider_mobile_content.html(content_html);
+    var active_slide = $('#hybrid');
 
     slider_mobile.slick({
         slidesToShow: 5,
@@ -16,14 +11,14 @@ $(document).ready(function(){
         centerPadding: 0,
         focusOnSelect: true
     });
-
-   /* slider_mobile.on('beforeChange', function() {
-        $('.bespoke__mobile__content__text').stop().animate({opacity: "0"}, 500);
+/*
+    slider_mobile.on('beforeChange', function() {
+        active_slide.stop().animate({opacity: "0"}, 500);
     });
 
     slider_mobile.on('afterChange', function(event, slick, slide) {
         $('.bespoke__mobile__content__text').stop().animate({opacity: "1"}, 500);
         $('.bespoke__mobile__content__text').text(slide);
-    });*/
-
+    });
+*/
 });
